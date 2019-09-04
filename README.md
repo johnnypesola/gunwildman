8643cff1af7525bfbea5c8d8505bcb82a84f81e3
Johnny Pesola <johnny@pesola.eu>
2019-09-04 23:00:41 +0200

    Initialize repo. Progress on startmenu

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Gun wildman
+
+A clone of the classic Wild gunman game using React.
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import gun from "./resources/img/gun.png";
+import wild from "./resources/img/wild.png";
+import man from "./resources/img/man.png";
+
+const Header = styled.header`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: #5a96ff;
+  text-transform: uppercase;
+  img {
+    margin: 20px 0;
+  }
+`;
+
+type LogoImgProps = {
+  src: string;
+};
+
+const LogoImg = ({ src }: LogoImgProps) => (
+  <img draggable={false} src={src} alt="logo" />
+);
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <LogoImg src={gun} />
+        <LogoImg src={wild} />
+        <LogoImg src={man} />
+      </Header>
+      <p>Game a</p>
+      <p>Game b</p>
+    </>
   );
-}
+};
 
 export default App;
