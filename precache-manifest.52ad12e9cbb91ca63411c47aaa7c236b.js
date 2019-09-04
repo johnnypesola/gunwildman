0ead2467a8f1ae6db92743c6f5111663195532d2
Johnny Pesola <johnny@pesola.eu>
2019-09-04 23:11:49 +0200

    Updates

---
+++ precache-manifest.52ad12e9cbb91ca63411c47aaa7c236b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62c05a7be3c3a0f85e44adda0bbaccc",
+    "url": "/gunwildman/index.html"
+  },
+  {
+    "revision": "4af5586fe1643ac0d265",
+    "url": "/gunwildman/static/css/main.52cbadb9.chunk.css"
+  },
+  {
+    "revision": "67ac79b27a3f2e66bae8",
+    "url": "/gunwildman/static/js/2.96827c10.chunk.js"
+  },
+  {
+    "revision": "4af5586fe1643ac0d265",
+    "url": "/gunwildman/static/js/main.4c0d713e.chunk.js"
+  },
+  {
+    "revision": "e8674cca7e71b8272b83",
+    "url": "/gunwildman/static/js/runtime~main.679615ee.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/gunwildman/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  }
+]);
